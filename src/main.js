49c31cd414f4b0a1aae40646bd9650570e747ee2
Chelsea Becker <cschweig2@gmail.com>
2020-11-16 13:44:20 -0800

    add updated search and random function
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>
    Co-authored-by: Tawnee Harris <tawneeh@icloud.com>

--- src/main.js
+++ src/main.js
@@ -4,26 +4,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    const city = $('#location').val();
-    $('#location').val("");
+  $('#gifResults').click(function() {
+    const userInput = $('#user-search').val();
+    $('#user-search').val("");
 
     let request = new XMLHttpRequest();
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${userInput}&limit=25&offset=0&rating=r&lang=en`;
 
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
         getElements(response);
       }
-    }
+    };
 
     request.open("GET", url, true);
     request.send();
 
-   function getElements(response) {
-      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    function getElements(response) {
+      $('.showGIF').html(`<iframe src="${response.data[0].embed_url}"></iframe>`);
+    }
+  });
+
+  $('#randomGif').click(function() {
+
+    let request2 = new XMLHttpRequest();
+    let urlRandom = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&rating=r&lang=en`;
+
+    request2.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request2.open("GET", urlRandom, true);
+    request2.send();
+
+    function getElements(response) {
+      $('.showRandom').html(`<iframe src="${response.data.embed_url}"></iframe>`);
     }
   });
 });
